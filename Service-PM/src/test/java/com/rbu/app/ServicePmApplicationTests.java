f96d7c23d651f61d9da04fb9eaa43d6234b02977
rbuenquiries <118588722+rbuenquiries@users.noreply.github.com>
2023-02-15 08:35:15 +0530

    Add files via upload

---
+++ Service-PM/src/test/java/com/rbu/app/ServicePmApplicationTests.java
@@ -0,0 +1,12 @@
+/*
+ * package com.rbu.app;
+ * 
+ * import org.junit.jupiter.api.Test; import
+ * org.springframework.boot.test.context.SpringBootTest;
+ * 
+ * @SpringBootTest class ServicePmApplicationTests {
+ * 
+ * @Test void contextLoads() { }
+ * 
+ * }
+ */
