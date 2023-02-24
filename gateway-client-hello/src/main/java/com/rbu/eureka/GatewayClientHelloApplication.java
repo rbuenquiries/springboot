322e2e0f44aab8446b44f2b4b1f006d8bdffd74f
rbuenquiries <118588722+rbuenquiries@users.noreply.github.com>
2023-02-24 08:42:29 +0530

    Add files via upload

---
+++ gateway-client-hello/src/main/java/com/rbu/eureka/GatewayClientHelloApplication.java
@@ -0,0 +1,20 @@
+package com.rbu.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GatewayClientHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayClientHelloApplication.class, args);
+	}
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello Message";
+	}
+
+}
---
+++ gateway-client-hello/src/test/java/com/rbu/eureka/GatewayClientHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbu.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayClientHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
